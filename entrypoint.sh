44202c244331c9c39dae6005f0a2010ab8415806
rilihuan <109838747+rilihuan@users.noreply.github.com>
2022-07-23 12:14:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,8 +39,8 @@ mkdir -p ${DIR_CONFIG}
 ${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
 # Install
-install -m 755 ${DIR_TMP}/vvray ${DIR_RUNTIME}
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run
-${DIR_RUNTIME}/vvray -config=${DIR_CONFIG}/config.pb
+${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
